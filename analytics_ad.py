f4eae9c4354a05e385b591d27ef80741b3a4a79a
zhouzhenliang <zhouzhenliangpp@163.com>
2024-12-12 21:27:11 +0800

    analytics

--- analytics_ad.py
+++ analytics_ad.py
@@ -4,7 +4,7 @@ import os.path
 import pandas as pd
 
 if __name__ == '__main__':
-    csv_path = '/Users/zhouzhenliang/Desktop/analytics_20241125/device_file_helper_ad.csv'
+    csv_path = '/Users/zhouzhenliang/Desktop/analytics_20241125/device_file_helper_1.csv'
     name1, name2 = os.path.splitext(csv_path)
     out_path = name1 + '_out' + name2
     df = pd.read_csv(csv_path)
@@ -24,7 +24,15 @@ if __name__ == '__main__':
         'business_loadactivity_viewed',
         'business_adactivity_viewed',
         'business_showinterstitialad',
-        'business_interstitialad_viewed'
+        'business_interstitialad_viewed',
+        'business_interstitialad_revenue',
+
+        'business_dowork_limit',
+        'business_interstitialad_loadtimeout',
+        'business_interstitialad_loadfailed',
+        'business_interstitialad_displayfailed',
+        'business_startactivity',
+        'business_activityviewed'
     ]
 
     # head
@@ -44,29 +52,45 @@ if __name__ == '__main__':
         for channel in channels:
             values = []
             for event in events:
+                find = False
                 for index, row in df.iterrows():
                     if (row.loc['event_date_utc'] == date and
                             row.loc['event_name'] == event and
                             row.loc['app_channel'] == channel):
+                        find = True
                         values.append((row.loc['event_count'], row.loc['user_count']))
                         break
+                if not find:
+                    values.append((0, 0))
             if len(values) != len(events):
                 continue
 
-            csv_rows = [date, channel.strip('"')]
+            csv_rows = [date, str(channel).strip('"')]
             for i in range(len(events)):
                 csv_rows.append(events[i])
                 event_count, user_count = values[i]
                 csv_rows.append(str(event_count))
                 csv_rows.append(str(user_count))
-                csv_rows.append(str(round(event_count / user_count, 1)))
+                if user_count == 0:
+                    csv_rows.append('0')
+                else:
+                    csv_rows.append(str(round(event_count / user_count, 1)))
 
             for i in range(1, len(events)):
                 event_count, user_count = values[i - 1]
                 event_count1, user_count1 = values[i]
-                ratio = event_count / user_count
-                ratio1 = event_count1 / user_count1
-                csv_rows.append(str(round(100 * ratio1 / ratio, 1)) + '%')
+                if user_count == 0:
+                    ratio = 0
+                else:
+                    ratio = event_count / user_count
+                if user_count1 == 0:
+                    ratio1 = 0
+                else:
+                    ratio1 = event_count1 / user_count1
+                if ratio == 0:
+                    csv_rows.append('0')
+                else:
+                    csv_rows.append(str(round(100 * ratio1 / ratio, 1)) + '%')
 
             csv_writer.writerow(csv_rows)
 
---
+++ analytics_startactivity.py
@@ -0,0 +1,55 @@
+import csv
+
+import pandas as pd
+
+ad_activity_names = ['com.anythink.basead.ui.ATLandscapeActivity',
+                     'com.mbridge.msdk.activity.MBCommonActivity',
+                     'com.bytedance.sdk.openadsdk.activity.TTWebsiteActivity',
+                     'com.mbridge.msdk.reward.player.MBRewardVideoActivity',
+                     'com.anythink.core.basead.ui.web.WebLandPageActivity',
+                     'com.bytedance.sdk.openadsdk.activity.TTFullScreenExpressVideoActivity',
+                     'com.facebook.ads.AudienceNetworkActivity',
+                     'com.bytedance.sdk.openadsdk.activity.TTFullScreenVideoActivity',
+                     'com.inmobi.ads.rendering.InMobiAdActivity',
+                     'sg.bigo.ads.api.AdActivity',
+                     'com.vungle.ads.internal.ui.VungleActivity',
+                     'com.bytedance.sdk.openadsdk.activity.TTLandingPageActivity',
+                     'sg.bigo.ads.api.CompanionAdActivity',
+                     'com.anythink.expressad.reward.player.ATRewardVideoActivity',
+                     'com.anythink.basead.ui.ATPortraitActivity']
+
+if __name__ == '__main__':
+    csv_path_1 = '/Users/zhouzhenliang/Desktop/temp/analytics-20241209/superstoragecleaner_startactivity_1.5.csv'
+    csv_path_2 = '/Users/zhouzhenliang/Desktop/temp/analytics-20241209/superstoragecleaner_activityviewed_1.5.csv'
+    out_path = '/Users/zhouzhenliang/Desktop/temp/analytics-20241209/superstoragecleaner_activity_out_1.5-1.csv'
+
+    df1 = pd.read_csv(csv_path_1)
+    df2 = pd.read_csv(csv_path_2)
+
+    csv_file = open(out_path, 'w')
+    csv_writer = csv.writer(csv_file)
+
+    headers = ['event_date_utc', 'component', 'start_event_count', 'view_event_count']
+    csv_writer.writerow(headers)
+
+    for i, row1 in df1.iterrows():
+        component = row1.loc['component'].strip('"')
+        if component not in ad_activity_names:
+            continue
+
+        date = row1.loc['event_date_utc']
+        event_count = row1.loc['event_count']
+        csv_rows = [date, component, event_count]
+
+        find = False
+        for j, row2 in df2.iterrows():
+            if row2.loc['event_date_utc'] == date and row2.loc['component'].strip('"') == component:
+                find = True
+                csv_rows.append(row2.loc['event_count'])
+                break
+        if not find:
+            csv_rows.append(0)
+
+        csv_writer.writerow(csv_rows)
+
+    csv_file.close()
