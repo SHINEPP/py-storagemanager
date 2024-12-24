e0c242e376c7ca62dd771419b9ac7871e247b1a0
zhouzhenliang <zhouzhenliangpp@163.com>
2024-12-24 20:55:42 +0800

    添加duration

--- gitsync/bitbucket_projects.py
+++ gitsync/bitbucket_projects.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import os
+import time
 
 import git
 import requests
@@ -55,22 +56,24 @@ def run():
         if os.path.exists(local_git):
             print(f'exist')
             repo = None
+            start_time = time.time()
             try:
                 print('origin pull')
                 repo = git.Repo(local_git)
                 repo.remote().pull()
-                print(f'origin pull success')
+                print(f'origin pull success, duration: {round(time.time() - start_time, 1)}s')
             except Exception as e:
-                logging.error(f'origin pull fail, e = {e}')
+                logging.error(f'origin pull fail, duration: {round(time.time() - start_time, 1)}s, e = {e}')
             finally:
                 repo.close()
         else:
             print(f'clone')
+            start_time = time.time()
             try:
                 git.Repo.clone_from(clone_url, local_git)
-                print(f'clone success')
+                print(f'clone success, duration: {round(time.time() - start_time, 1)}s')
             except Exception as e:
-                logging.error(f'clone fail, e = {e}')
+                logging.error(f'clone fail, duration: {round(time.time() - start_time, 1)}s, e = {e}')
         print('---------------------------------------')
         index += 1
 
