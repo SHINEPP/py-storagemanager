22d3f33d7d29e640dda70e53154c64144099350c
zhouzhenliang <zhouzhenliangpp@163.com>
2024-12-26 15:08:35 +0800

    多线程下载

--- gradle/gradle_downloader.py
+++ gradle/gradle_downloader.py
@@ -1,6 +1,7 @@
 import hashlib
 import os.path
 import sys
+import threading
 import time
 
 import requests
@@ -10,68 +11,82 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
 
-def fetch_gradle_distributions(url):
-    """
-    使用 Selenium 爬取动态网页
-    """
-    # 配置 Chrome WebDriver
-    chrome_options = Options()
-    chrome_options.add_argument('--headless')  # 无界面模式
-    service = Service('/Users/zhouzhenliang/bin/chromedriver-mac-x64/chromedriver')
+class GradleDistributions:
 
-    # 启动 WebDriver
-    driver = webdriver.Chrome(service=service, options=chrome_options)
-    driver.get(url)
+    def __init__(self):
+        self.distributions = []
+        self.host_url = 'https://services.gradle.org/distributions/'
+        self.gradle_dir = '/Volumes/WDDATA/gradle/distributions'
+        self.download_index = 0
+        self.lock = threading.Lock()
 
-    # 等待加载完成并提取内容
-    title = driver.title
-    print(f'Page Title: {title}')
+    def start(self):
+        self._fetch_distributions()
+        self.download_index = 0
+        print(f'distributions count: {len(self.distributions)}')
+        threads = []
+        for i in range(4):
+            t = threading.Thread(target=self._download_distributions, args=())
+            threads.append(t)
+            t.start()
+        for t in threads:
+            t.join()
 
-    contents = driver.find_element(By.ID, 'contents')
-    if contents:
-        items = contents.find_elements(By.CLASS_NAME, 'items')
-        for item in items:
-            links = item.find_elements(By.TAG_NAME, 'a')
-            for link in links:
-                href = link.get_attribute('href')
-                if href:
-                    print(href)
-                    download_gradle_distribution_file(href)
+    def _download_distributions(self):
+        while True:
+            with self.lock:
+                index = self.download_index
+                self.download_index += 1
+            if index >= len(self.distributions):
+                break
+            print(f'index: {index}')
+            self._download_distribution(self.distributions[index])
 
-    # 关闭浏览器
-    driver.quit()
+    def _fetch_distributions(self):
+        self.distributions.clear()
 
+        # chromedriver
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')  # 无界面模式
+        service = Service('/Users/zhouzhenliang/bin/chromedriver-mac-x64/chromedriver')
+        driver = webdriver.Chrome(service=service, options=chrome_options)
 
-def download_gradle_distribution_file(url: str):
-    gradle_dir = '/Volumes/WDDATA/gradle/distributions'
-    dir_path, name = os.path.split(url)
-    dst_path = os.path.join(gradle_dir, name)
-    if os.path.exists(dst_path):
-        print(f'file exist')
-        return
+        driver.get(self.host_url)
+        contents = driver.find_element(By.ID, 'contents')
+        if not contents:
+            return
+        item = contents.find_element(By.CLASS_NAME, 'items')
+        if not item:
+            return
+        links = item.find_elements(By.TAG_NAME, 'a')
+        for link in links:
+            href = link.get_attribute('href')
+            if not href:
+                continue
+            self.distributions.append(href)
 
-    start_time = time.time()
-    try:
-        print(f'start download, dst: {dst_path}')
-        response = requests.get(url, stream=True)  # 开启流式下载
-        response.raise_for_status()  # 检查请求是否成功
-        with open(dst_path, 'wb') as file:
-            for chunk in response.iter_content(chunk_size=8192):  # 分块写入
-                file.write(chunk)
-        print(f'download success, duration: {round(time.time() - start_time, 2)}s')
-    except requests.RequestException as e:
-        os.remove(dst_path)
-        print(f'download fail, duration: {round(time.time() - start_time, 2)}s, e: {e}', file=sys.stderr)
+    def _download_distribution(self, url: str):
+        dir_path, name = os.path.split(url)
+        dst_path = os.path.join(self.gradle_dir, name)
+        if os.path.exists(dst_path):
+            print(f'file exist, path: {dst_path}')
+            return
 
-
-def calculate_sha256(path):
-    sha256_hash = hashlib.sha256()
-    with open(path, 'rb') as file:
-        for chunk in iter(lambda: file.read(8192), b''):
-            sha256_hash.update(chunk)
-    return sha256_hash.hexdigest()
+        start_time = time.time()
+        try:
+            response = requests.get(url, stream=True)  # 开启流式下载
+            response.raise_for_status()  # 检查请求是否成功
+            with open(dst_path, 'wb') as file:
+                for chunk in response.iter_content(chunk_size=8192):  # 分块写入
+                    file.write(chunk)
+            print(f'download success, path: {dst_path}, duration: {round(time.time() - start_time, 2)}s')
+        except requests.RequestException as e:
+            if os.path.exists(dst_path):
+                os.remove(dst_path)
+            print(f'download fail, , path: {dst_path}, duration: {round(time.time() - start_time, 2)}s, e: {e}',
+                  file=sys.stderr)
 
 
 if __name__ == '__main__':
-    URL = 'https://services.gradle.org/distributions/'  # 替换为目标 URL
-    fetch_gradle_distributions(URL)
+    impl = GradleDistributions()
+    impl.start()
