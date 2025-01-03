e993692830148f1aca714fa7bc5a8e6a67ee9cf8
zhouzhenliang <zhouzhenliangpp@163.com>
2025-01-03 16:12:53 +0800

    test

--- aosp/aosp_walk_git.py
+++ aosp/aosp_walk_git.py
@@ -1,10 +1,11 @@
 import os
+import xml.etree.ElementTree as ET
 
 import git
 
 
 def walk_git(root_dir, index=0):
-    if index >= 5:
+    if index >= 10:
         return
     if not os.path.isdir(root_dir):
         return
@@ -19,16 +20,36 @@ def walk_git(root_dir, index=0):
 
 
 def walk_gits():
-    aosp_root_dir = '/Volumes/WDDATA/android/aosp/android-repo'
+    aosp_root_dir = '/Volumes/WDDATA4T/android/aosp/android-latest'
+
     index = 0
     for proj_path in walk_git(aosp_root_dir):
+        index += 1
         path = proj_path[len(aosp_root_dir):].strip(os.path.sep)
         repo = git.Repo(proj_path)
         commit = repo.head.commit
         tags = [tag.name for tag in repo.tags if tag.commit == commit]
         name = ', '.join(tags)
         print(f'index: {index}, path: {path}, name: {name}')
-        index += 1
+
+
+def browser_gits():
+    aosp_root_dir = '/Volumes/WDDATA4T/android/aosp/android-latest'
+    manifest = os.path.join(aosp_root_dir, '.repo', 'manifests', 'default.xml')
+    with open(manifest, 'r') as file:
+        root = ET.fromstring(file.read())
+
+    index = 0
+    for child in root:
+        if child.tag == 'project':
+            index += 1
+            path = child.get('path')
+            proj_path = os.path.join(aosp_root_dir, path)
+            repo = git.Repo(proj_path)
+            commit = repo.head.commit
+            tags = [tag.name for tag in repo.tags if tag.commit == commit]
+            name = ', '.join(tags)
+            print(f'index: {index}, path: {path}, name: {name}')
 
 
 if __name__ == '__main__':
