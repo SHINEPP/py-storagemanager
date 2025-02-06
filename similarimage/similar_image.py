d341c56ffb2585bc2391caaa8ec9b8b2a5508039
zhouzhenliang <zhouzhenliangpp@163.com>
2025-02-06 14:59:04 +0800

    fixbug

--- similarimage/similar_image.py
+++ similarimage/similar_image.py
@@ -1,29 +1,38 @@
-import tensorflow as tf
+import os
+import shutil
+
 import numpy as np
+import tensorflow as tf
 from numpy.linalg import norm
 
-# 加载 TFLite 模型
-interpreter = tf.lite.Interpreter(model_path="mobilenet_v2.tflite")
-interpreter.allocate_tensors()
 
-# 获取输入/输出张量信息
-input_details = interpreter.get_input_details()
-output_details = interpreter.get_output_details()
+class ImageFeatures:
 
-print("输入张量:", input_details)
-print("输出张量:", output_details)
+    def __init__(self):
+        # 加载 TFLite 模型
+        self.interpreter = tf.lite.Interpreter(model_path="mobilenet_v2.tflite")
+        self.interpreter.allocate_tensors()
 
+        # 获取输入/输出张量信息
+        self.input_details = self.interpreter.get_input_details()
+        self.output_details = self.interpreter.get_output_details()
 
-def extract_features(image):
-    """ 计算图片的特征向量 """
-    img = tf.image.resize(image, (224, 224))  # 调整输入尺寸
-    img = tf.expand_dims(img, axis=0) / 255.0  # 归一化
+        print("输入张量:", self.input_details)
+        print("输出张量:", self.output_details)
 
-    interpreter.set_tensor(input_details[0]['index'], img.numpy().astype(np.float32))
-    interpreter.invoke()  # 运行推理
-    feature_vector = interpreter.get_tensor(output_details[0]['index'])
+    def extract_vector(self, image_path):
+        """ 计算图片的特征向量 """
+        img = tf.io.read_file(image_path)
+        img = tf.image.decode_jpeg(img, channels=3)
 
-    return feature_vector.flatten()
+        img = tf.image.resize(img, (224, 224))  # 调整输入尺寸
+        img = tf.expand_dims(img, axis=0) / 255.0  # 归一化
+
+        self.interpreter.set_tensor(self.input_details[0]['index'], img.numpy().astype(np.float32))
+        self.interpreter.invoke()  # 运行推理
+        feature_vector = self.interpreter.get_tensor(self.output_details[0]['index'])
+
+        return feature_vector.flatten()
 
 
 def cosine_similarity(vec1, vec2):
@@ -31,18 +40,36 @@ def cosine_similarity(vec1, vec2):
     return np.dot(vec1, vec2) / (norm(vec1) * norm(vec2))
 
 
-# 加载两张图片
-img1 = tf.io.read_file("/Users/zhouzhenliang/Desktop/tensorflowlite/data/image_01.jpg")
-img1 = tf.image.decode_jpeg(img1, channels=3)
+def main_test():
+    features = ImageFeatures()
+    results = []
 
-img2 = tf.io.read_file("/Users/zhouzhenliang/Desktop/tensorflowlite/data/image_02.jpg")
-img2 = tf.image.decode_jpeg(img2, channels=3)
+    img_dir = '/Users/zhouzhenliang/Desktop/tensorflowlite/Image'
+    img_out_dir = '/Users/zhouzhenliang/Desktop/tensorflowlite/Image_out'
+    for root, dirs, files in os.walk(img_dir):
+        for file in files:
+            path = os.path.join(root, file)
+            values = features.extract_vector(path)
+            results.append({'path': path, 'values': values})
 
-# 提取特征向量
-feature1 = extract_features(img1)
-feature2 = extract_features(img2)
+    if os.path.exists(img_out_dir):
+        shutil.rmtree(img_out_dir)
 
-# 计算相似度
-# 相似度值接近 1 代表两张图片相似，接近 0 代表不相似。
-similarity = cosine_similarity(feature1, feature2)
-print("相似度:", similarity)
+    count = len(results)
+    for i in range(count):
+        for j in range(i + 1, count):
+            path1 = results[i]['path']
+            path2 = results[j]['path']
+            name1 = os.path.split(path1)[-1]
+            name2 = os.path.split(path2)[-1]
+            similarity = cosine_similarity(results[i]['values'], results[j]['values'])
+            print(f'{i} - {j}, 相似度: {similarity}, {name1} - {name2}')
+            if similarity > 0.95:
+                out_dir = os.path.join(img_out_dir, f'V{similarity}')
+                os.makedirs(out_dir, exist_ok=True)
+                shutil.copyfile(path1, os.path.join(out_dir, name1))
+                shutil.copyfile(path2, os.path.join(out_dir, name2))
+
+
+if __name__ == '__main__':
+    main_test()
