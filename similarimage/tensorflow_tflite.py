81356e2ef40c24ad32427cc409a22ea6d0e9f46f
zhouzhenliang <zhouzhenliangpp@163.com>
2025-02-06 16:15:04 +0800

    TTT

--- similarimage/tensorflow_tflite.py
+++ similarimage/tensorflow_tflite.py
@@ -9,10 +9,10 @@ ssl._create_default_https_context = ssl._create_unverified_context
 # 保存模型
 # model.export("mobilenet_v2_001")
 
-converter = tf.lite.TFLiteConverter.from_saved_model("/Users/zhouzhenliang/Desktop/tensorflowlite/model/mobilenet-v3-tensorflow2-large-075-224-classification-v1")
+converter = tf.lite.TFLiteConverter.from_saved_model("/Users/zhouzhenliang/Desktop/tensorflowlite/model/mobilenet-v1-tensorflow2-025-128-classification-v2")
 tflite_model = converter.convert()
 
 # 保存 TFLite 模型
-with open("mobilenet-v3-tensorflow2-large-075-224-classification-v1.tflite", "wb") as f:
+with open("mobilenet-v1-tensorflow2-025-128-classification-v2.tflite", "wb") as f:
     f.write(tflite_model)
 print("转换完成！")
