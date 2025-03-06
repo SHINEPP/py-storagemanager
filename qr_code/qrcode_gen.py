55595e01f7a857f38ce5a5e14fd2228a78d3dd62
zhouzhenliang <zhouzhenliangpp@163.com>
2025-03-06 11:18:56 +0800

    qrcode

---
+++ qr_code/qrcode_gen.py
@@ -0,0 +1,19 @@
+from qrcode.main import QRCode
+
+
+def gen_qrcode(data: str):
+    qr = QRCode(
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(data)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+
+    with open('build/output.png', 'wb') as f:
+        img.save(f)
+
+
+if __name__ == '__main__':
+    gen_qrcode('https://play.google.com/store/apps/details?id=com.wheresmyphone.eltools')
